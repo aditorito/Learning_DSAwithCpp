5790fd5e87bb11493eb248864c568ddff808cc3a
Aditya <as0742004@gmail.com>
2025-01-31 23:17:18 +0530

    Lecture 4 competed

--- L3Patter/test.cpp
+++ L3Patter/test.cpp
@@ -1,20 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
     int n;
+
     cin >> n;
     for (int row = 1; row <= n; row++)
     {
-        for (int col = 1; col <= row; col++)
-        {
-            int count = 1;
-            cout<<count<< " ";
-            count++;
-        }
-        cout<<endl;
         
+         for (int col = 1; col <= n - row; col++)
+        {
+            cout << " ";
+        }
+        for (int star = 1; star <= row  ; star++)
+        {
+            cout<< star;
+        }
+        for (int sid = 1; sid < row; sid++)
+        {
+            cout<<row - sid ;
+            
+        }
+        
+        cout<<endl;
+
+       
     }
-    
 }
