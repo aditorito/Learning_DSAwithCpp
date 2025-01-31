e697c2c15effa74704ef011bbf6e613efbed124f
Aditya <as0742004@gmail.com>
2025-01-31 23:18:47 +0530

    Starting lecture 5

--- test.cpp
+++ test.cpp
@@ -2,9 +2,27 @@
 using namespace std;
 
 int main() {
-    int a = 5;
+    int a = 10;
   
     cout << a << endl;
 
+
+    if (a > 0)
+    {
+        cout<<"positive"<<endl;
+    }else if(a < 0){
+        cout<<"negative"<<endl;
+    }else{
+        cout<<"mera maan"<<endl;
+    }
+
+    
+
+    
+    
+
+    
     return 0;
+
+    
 }
