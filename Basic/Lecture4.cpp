36c7f2149b3cc341b55564227be50fc5f8ca34c8
Aditya <as0742004@gmail.com>
2025-02-01 18:31:46 +0530

    Lecture 6 is completed

---
+++ Basic/Lecture4.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    // int n;
+    // cin >> n;
+    // int first = 0;
+    // int second = 1;
+    // int sum ;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     sum = first + second;
+    //     first = second;
+    //     cout<<first<<endl;
+    //     second = sum;
+    //     cout<<second<<endl;
+    // }
+
+    // cout<<sum<<endl;
+
+    // int n;
+
+    // cin >> n;
+
+    // if (n <= 1)
+    // {
+    //     cout<<"Prime";
+    // }else{
+    //     for (int c = 2; c * c < n; c++)
+    //     {
+    //         cout << c << " This number is being used"<<endl;
+    //         if (n % c == 0)
+    //         {
+    //             cout<<"Not a Prime";
+    //             break;
+    //         }
+
+    //     }
+    //     cout<<"Prime number";
+
+    // }
+    int n;
+    cin >> n;
+    int sum = 0;
+    int i  = 0;
+    while (n != 0)
+    {
+        int digit = n % 10;
+        if (digit == 1)
+        {
+            sum = sum  + pow(2,i);
+
+        }
+
+        n  = n / 10;
+        i++;
+        
+    }
+    cout<<sum<<endl;
+    
+}
